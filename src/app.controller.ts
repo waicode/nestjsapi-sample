1cdff6efd4d3a06ebf32d7ad2ba11da66392cb79
waicode <waicode37@gmail.com>
2021-09-25 16:47:19 +0900

    fix: delete qiita controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
-import { map, Subject } from 'rxjs';
-import { AppService } from './app.service';
 import { QiitaService } from './qiita/qiita.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly qiitaService: QiitaService,
-  ) {}
+  constructor(private readonly qiitaService: QiitaService) {}
 
   @Get()
   getQiitaItem(): any {
--- src/qiita/qiita.module.ts
+++ src/qiita/qiita.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { QiitaController } from './qiita.controller';
 import { QiitaService } from './qiita.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [QiitaController],
   providers: [QiitaService],
   exports: [QiitaService],
 })
